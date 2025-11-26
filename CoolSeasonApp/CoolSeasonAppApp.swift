0f91c35e2ed2c5d9da1f6f57366e5c4be76707ea
Halis Can <haliscan@hotmail.com>
2025-11-26 13:27:09 -0500

    Initial Commit

---
+++ CoolSeasonApp/CoolSeasonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolSeasonAppApp.swift
+//  CoolSeasonApp
+//
+//  Created by Halis Can on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CoolSeasonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
